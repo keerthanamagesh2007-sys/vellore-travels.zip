aeaac4f1f9e71f68ddacd27902ec1e320d1c4e6f
keerthanamagesh2007-sys <keerthana.magesh2007@gmail.com>
2026-06-18 10:58:32 +0530

    Add files via upload

---
+++ left.php
@@ -0,0 +1,30 @@
+<div class="grid5">
+<h2><em></em><a href="tourist-places.php" target="_blank"> Tourist Places</a></h2>
+    <ul class="products">
+<li><a href="vellore-fort.php" target="_blank">Vellore Fort </a></li>
+<li><a href="govt-museum.php" target="_blank">Government Museum </a></li>
+<li><a href="observatory.php" target="_blank">Kavalur Observatory </a></li>
+<li><a href="yelagiri.php" target="_blank"> Yelagiri </a></li>
+<li><a href="fundera-park.php" target="_blank"> Fundera Park </a></li>
+<li><a href="amrithi-forest.php" target="_blank"> Amirthi Forest</a></li>
+<li><a href="jalagamparai-falls.php" target="_blank">Jalagamparai Falls</a></li>
+<li><a href="st-johns-church.php" target="_blank">St. John's Church</a></li>
+<li><a href="assumption-cathedral.php" target="_blank">Assumption Cathedral</a></li>
+<li><a href="periyar-park.php" target="_blank">Periyar Park</a></li>
+<li><a href="water-park.php" target="_blank">Water Park</a></li>
+<li><a href="jalakandeswarar-temple.php" target="_blank">Jalakandeswarar Temple</a></li>
+</ul>
+<div class="space"> &nbsp;</div>
+<div class="space"> &nbsp;</div>
+<h2><em></em> <a href="golden-temple.php" target="_blank">Golden Temples</a></h2>
+<a href="golden-temple.php" target="_blank"><img src="images/golden-temple.gif" alt="Golden Temple"  style="margin:5px;"/></a>
+  <!--  <ul class="products">
+<li><a href="#.php">Bala Murugan Temple </a></li>
+<li><a href="#.php">Murugar Temple </a></li>
+<li><a href="#.php"> Padavedu </a></li>
+<li style="background-image:none"><a href="#.php"> Ratnagiri Temple</a></li>
+
+</ul>-->
+
+
+  </div>
